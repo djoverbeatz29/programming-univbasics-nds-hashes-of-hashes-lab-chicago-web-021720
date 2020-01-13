3ed44ab7f4fea7a9bbe3e56d8427bbca40f9aa35
Jack Overby <jroverby92@gmail.com>
2020-01-13 05:59:27 +0000

    Automatically backed up by Learn

---
+++ Fuque/Reche.rb
@@ -0,0 +1,10 @@
+def naming_system
+  hierarchy = ["Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"]
+  N = hierarchy.length - 1
+  reche = {:label => hierarchy[N], :sub_category => nil}
+  for i in 0...N do
+    reche = {:label => hierarchy[N-1-i], :sub_category => reche}
+  end
+  reche
+end
+
